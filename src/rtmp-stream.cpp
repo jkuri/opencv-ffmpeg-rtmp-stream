5262d1a5ad484fae4f8428b0c6f4565a91d2972a
Jan Kuri <jkuri88@gmail.com>
2018-02-04 12:03:28 +0100

    chore(): add sps and pps to out_codec_ctx

--- src/rtmp-stream.cpp
+++ src/rtmp-stream.cpp
@@ -13,6 +13,14 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
+struct buffer_data
+{
+  uint8_t *buf;
+  size_t size;
+  uint8_t *ptr;
+  size_t room; ///< size left in the buffer
+};
+
 cv::VideoCapture get_device(int camID, double width, double height)
 {
   cv::VideoCapture cam(camID);
@@ -56,7 +64,8 @@ void set_codec_params(AVFormatContext *&fctx, AVCodecContext *&codec_ctx, double
   const AVRational dst_fps = {fps, 1};
 
   codec_ctx->codec_tag = 0;
-  codec_ctx->bit_rate = 400000;
+  codec_ctx->codec_id = AV_CODEC_ID_H264;
+  codec_ctx->codec_type = AVMEDIA_TYPE_VIDEO;
   codec_ctx->width = width;
   codec_ctx->height = height;
   codec_ctx->time_base.den = fps;
@@ -65,10 +74,10 @@ void set_codec_params(AVFormatContext *&fctx, AVCodecContext *&codec_ctx, double
   codec_ctx->pix_fmt = AV_PIX_FMT_YUV420P;
   codec_ctx->framerate = dst_fps;
   codec_ctx->time_base = av_inv_q(dst_fps);
-  if (fctx->oformat->flags & AVFMT_GLOBALHEADER)
-  {
-    codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
-  }
+  // if (fctx->oformat->flags & AVFMT_GLOBALHEADER)
+  // {
+  //   codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+  // }
 }
 
 void initialize_codec_stream(AVStream *&stream, AVCodecContext *&codec_ctx, AVCodec *&codec)
@@ -84,6 +93,7 @@ void initialize_codec_stream(AVStream *&stream, AVCodecContext *&codec_ctx, AVCo
   av_dict_set(&codec_options, "profile", "high", 0);
   av_dict_set(&codec_options, "preset", "superfast", 0);
   av_dict_set(&codec_options, "tune", "zerolatency", 0);
+  av_dict_set(&codec_options, "threads", "1", 0);
 
   // open video encoder
   ret = avcodec_open2(codec_ctx, codec, &codec_options);
@@ -119,12 +129,33 @@ AVFrame *allocate_frame_buffer(AVCodecContext *codec_ctx, double width, double h
   return frame;
 }
 
+void hexdump(void *ptr, int buflen)
+{
+  unsigned char *buf = (unsigned char *)ptr;
+  int i, j;
+  for (i = 0; i < buflen; i += 16)
+  {
+    printf("%06x: ", i);
+    for (j = 0; j < 16; j++)
+      if (i + j < buflen)
+        printf("%02x ", buf[i + j]);
+      else
+        printf("   ");
+    printf(" ");
+    for (j = 0; j < 16; j++)
+      if (i + j < buflen)
+        printf("%c", isprint(buf[i + j]) ? buf[i + j] : '.');
+    printf("\n");
+  }
+}
+
 void stream_video(double width, double height, int fps, int camID)
 {
   av_register_all();
   avformat_network_init();
 
   const char *output = "rtmp://localhost/live/stream";
+  // const char *output = "/Users/jan/Desktop/file.flv";
   int ret;
   auto cam = get_device(camID, width, height);
   std::vector<uint8_t> imgbuf(height * width * 3 + 16);
@@ -141,11 +172,26 @@ void stream_video(double width, double height, int fps, int camID)
   out_stream = avformat_new_stream(ofmt_ctx, out_codec);
   out_codec_ctx = avcodec_alloc_context3(out_codec);
 
+  // unsigned char sps_pps[23] = { 0x00, 0x00, 0x00, 0x01, 0x67, 0x64, 0x00, 0x29, 0xac, 0x1b, 0x1a, 0x12, 0xe0, 0x51, 0x90, 0x00, 0x00, 0x00, 0x01, 0x68, 0xea, 0x43, 0xcb };
+  unsigned char sps_pps[23] = { 0x00, 0x00, 0x00, 0x01, 0x67, 0x42, 0x00, 0x0a, 0xf8, 0x41, 0xa2, 0x00, 0x00, 0x00, 0x01, 0x68, 0xce, 0x38, 0x80 };
+  out_codec_ctx->extradata_size = 23;
+  out_codec_ctx->extradata = (uint8_t*)av_malloc(23 + AV_INPUT_BUFFER_PADDING_SIZE);
+  if (out_codec_ctx->extradata == NULL) {
+    printf("could not av_malloc the video params extradata!\n");
+    exit(1);
+  }
+  memcpy(out_codec_ctx->extradata, sps_pps, 23);
+
   set_codec_params(ofmt_ctx, out_codec_ctx, width, height, fps);
   initialize_codec_stream(out_stream, out_codec_ctx, out_codec);
 
+  av_dump_format(ofmt_ctx, 0, output, 1);
+
   auto *swsctx = initialize_sample_scaler(out_codec_ctx, width, height);
   auto *frame = allocate_frame_buffer(out_codec_ctx, width, height);
+
+  int cur_size;
+  uint8_t *cur_ptr;
 
   ret = avformat_write_header(ofmt_ctx, nullptr);
   if (ret < 0)
@@ -161,17 +207,12 @@ void stream_video(double width, double height, int fps, int camID)
 
   do
   {
-    nb_frames++;
+    cam >> image;
+    const int stride[] = {static_cast<int>(image.step[0])};
+    sws_scale(swsctx, &image.data, stride, 0, image.rows, frame->data, frame->linesize);
+    frame->pts = frame_pts++;
 
-    if (!end_of_stream)
-    {
-      cam >> image;
-      const int stride[] = {static_cast<int>(image.step[0])};
-      sws_scale(swsctx, &image.data, stride, 0, image.rows, frame->data, frame->linesize);
-      frame->pts = frame_pts++;
-    }
-
-    AVPacket pkt = { 0 };
+    AVPacket pkt = {0};
     av_init_packet(&pkt);
 
     ret = avcodec_send_frame(out_codec_ctx, frame);
@@ -188,11 +229,12 @@ void stream_video(double width, double height, int fps, int camID)
       exit(1);
     }
 
-    av_packet_rescale_ts(&pkt, out_codec_ctx->time_base, out_stream->time_base);
-    pkt.pos = -1;
+    hexdump(pkt.data, sizeof(pkt.data));
+
     av_interleaved_write_frame(ofmt_ctx, &pkt);
 
     av_packet_unref(&pkt);
+    nb_frames++;
   } while (!end_of_stream);
 
   av_write_trailer(ofmt_ctx);
@@ -208,7 +250,7 @@ int main()
 {
   av_log_set_level(AV_LOG_DEBUG);
   double width = 640, height = 480;
-  int camID = 1, fps = 30;
+  int camID = 1, fps = 25;
 
   stream_video(width, height, fps, camID);
 
